60e98d8093ae4ffc26aa598ad65c683a5206a203
theMitch36 <devmakos36@gmail.com>
2018-03-10 17:07:39 -0500

    basic working react-redux auth using HOCs

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Secret Shit
+            <ul>
+                <li>Secret 1</li>
+                <li>Secret 2</li>
+                <li>Secret 3</li>
+            </ul>
+        </div>
+    );
+};
+
